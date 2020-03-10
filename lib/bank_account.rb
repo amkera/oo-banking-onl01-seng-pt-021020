726d9df96e0205a24919bd28965aab28f2ec213c
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-10 05:15:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class BankAccount
+  attr_reader :name 
+  attr_accessor :balance, :status
+  
+  def initialize(name, balance = 1000, status = "open")
+    @name = name
+    @balance = balance 
+    @status = status
+  end 
+  
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+  
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end 
+  
+  def valid?
+    #self is bank account 
+    self.status == "open" && self.balance > 0 ? true : false 
+    #binding.pry
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Transfer
-  # your code here
-end
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount 
+    @status = status
+  end 
+  
+  def valid?
+    #self is an instance of Transfer 
+    self.sender.valid? && self.receiver.valid?
+    #Return true if both the sender's accounts and the receiver's transfer are valid. 
+  end 
+  
+  def execute_transaction
+    if sender.balance > @amount && @status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount 
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end 
+  end 
+      
+end 
