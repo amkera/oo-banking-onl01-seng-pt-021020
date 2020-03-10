d344739e8f0827a32e5ad9842119419103b4cecb
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-10 05:26:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,6 @@ class Transfer
   
   def execute_transaction
     if valid? && sender.balance > amount && @status == "pending"
-      
       sender.balance -= amount 
       receiver.balance += amount
       @status = "complete"
@@ -26,6 +25,9 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     end 
   end 
+  
+  def reverse_transfer
+    
       
     
       
