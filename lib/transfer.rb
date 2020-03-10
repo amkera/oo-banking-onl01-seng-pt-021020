ff3cd8d1a3461e0dbf3a973ce832c6425959bec7
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-10 05:20:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,10 @@ class Transfer
     #else 
       #rejected 
     if valid? && sender.balance > amount 
+      @status == "pending"
+      sender.balance -= amount 
+      receiver.balance += amount
+      @status = "complete"
       
     
       
