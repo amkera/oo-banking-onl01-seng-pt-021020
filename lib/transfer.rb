8098fce313d43e3881ad2de405096567bcef9d62
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-10 05:18:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,18 +12,17 @@ class Transfer
   def valid?
     #self is an instance of Transfer 
     self.sender.valid? && self.receiver.valid?
-    #Return true if both the sender's accounts and the receiver's transfer are valid. 
+    #Return true if both the sender's accounts and the receiver's transfer are valid in that both accounts are open, with balance greater than 0
   end 
   
   def execute_transaction
-    if sender.balance > @amount && @status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount 
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end 
-  end 
+    #if sender and receiver accounts are valid and sender has the money
+      #perform the transfer 
+      #status is complete
+    #else 
+      #rejected 
+    if valid? && sender.amount
+      
+    
       
 end 
